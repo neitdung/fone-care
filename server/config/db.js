9710a9c63adc5486e0c9f7fc895f9633b1047194
Neit Gnud <neitgnud@gmail.com>
2021-12-14 01:14:53 +0700

    remove secret key

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 exports.connectToDb = () => {
     mongoose
-    .connect("mongodb+srv://dungnt:DungNT123@cluster0.cpxco.mongodb.net/fone-care?retryWrites=true&w=majority", 
+    .connect(MONGO_URI, 
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Mongo Connection successful"))
     .catch(err => console.log(err));
