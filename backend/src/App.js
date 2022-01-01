3ab2e9681a0e2e8bf5f3e563984ae0bb2878939a
Neit Gnud <neitgnud@gmail.com>
2022-01-01 16:55:13 +0700

    install pwa for backend

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { Drawer, Layout, Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        {/* <Layout.Header>
+          <Header />
+        </Layout.Header> */}
+        <Layout.Content>
+          {/* <Routes>
+             <Route path="customer" element={<Dashboard />}>
+              <Route path="wishlist" element={<Dashboard />} />
+              <Route path="address" element={<Dashboard />} />
+              <Route path="info" element={<Dashboard />} />
+              <Route path="review" element={<Dashboard />} />
+              <Route path="order" element={<Dashboard />} />
+            </Route>
+            <Route path="signin" element={<Dashboard />} />
+            <Route path="signup" element={<Dashboard />} />
+            <Route path="category/:slug" element={<Dashboard />} />
+            <Route path="product/:slug" element={<Dashboard />} />
+            <Route path="search" element={<Dashboard />} />
+            <Route path="cart" element={<Dashboard />} />
+            <Route path="checkout" element={<Dashboard />}>
+              <Route path="success" element={<Dashboard />} />
+            </Route>
+
+          </Routes> */}
+            <div>Content</div>
+        </Layout.Content>
+        <Layout.Footer>
+          <div>footer</div>
+        </Layout.Footer>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
